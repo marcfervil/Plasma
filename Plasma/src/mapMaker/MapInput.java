9c6b61f6e2f8c9c35a12b78f203cc4f0a86d979c
Adam <Diamondrubix@gmail.com>
2017-01-18 11:59:18 -0600

    code syin test

---
+++ Plasma/src/mapMaker/MapInput.java
@@ -0,0 +1,86 @@
+package mapMaker;
+
+import java.util.ArrayList;
+
+import org.lwjgl.input.Keyboard;
+
+import com.capstone.plasma.player.Player;
+import com.capstone.plasma.player.PlayerHandler;
+import org.lwjgl.input.Keyboard;
+
+public class MapInput {
+	public static int scrollSpeed = 3;
+
+	
+	static ArrayList<Integer> keysDown = new ArrayList<Integer>();
+	
+	
+	public static void get(){
+		//System.out.println("get ran");
+		while(Keyboard.next()){
+			if(Keyboard.getEventKeyState()){
+				if(!keysDown.contains(Keyboard.getEventKey()))keysDown.add(Keyboard.getEventKey());
+			}else{
+				keysDown.remove(keysDown.indexOf(Keyboard.getEventKey()));
+		    }
+		}
+	}
+	
+	public static void startKeyManager(){
+		new KeyManager().start();
+		//System.out.println("test");
+	}
+	
+	static class KeyManager extends Thread{
+		
+		public void run(){
+			//System.out.println("test");
+			while(true){
+				//mapScreen.moveCam();
+				
+				try {
+					Thread.sleep(10);
+				} catch (InterruptedException e) {
+				}
+				//for(int key:keysDown){
+				for(int i=0;i<keysDown.size();i++){
+					int key=keysDown.get(i);
+			//		System.out.println(Player.x);
+					switch(key){
+						
+						case Keyboard.KEY_RIGHT:
+							//mapScreen.xCam+=1;
+							mapScreen.moveCam(scrollSpeed, 0);
+							break;
+						case Keyboard.KEY_LEFT:
+							mapScreen.moveCam(-scrollSpeed, 0);
+							//mapScreen.xCam+=1;
+								//mapScreen.backCam+=2
+							break;
+						case Keyboard.KEY_UP:
+							mapScreen.moveCam(0, scrollSpeed);
+							//mapScreen.yCam+=1;
+							break;
+						case Keyboard.KEY_D:
+							//mapScreen.xCam+=1;
+							mapScreen.moveCam(scrollSpeed, 0);
+							break;
+						case Keyboard.KEY_A:
+							mapScreen.moveCam(-scrollSpeed, 0);
+							//mapScreen.xCam-=1;
+							break;
+						case Keyboard.KEY_S:
+							mapScreen.moveCam(0, scrollSpeed);
+							//mapScreen.yCam+=1;
+							break;
+						case Keyboard.KEY_W:
+							mapScreen.moveCam(0, -scrollSpeed);
+							//mapScreen.yCam-=1;
+							break;
+						
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Plasma/src/mapMaker/mapScreen.java
@@ -0,0 +1,44 @@
+package mapMaker;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+import com.capstone.plasma.player.PlayerHandler;
+import com.capstone.plasma.player.Player;
+import com.capstone.plasma.tiles.Tile;
+
+import static org.lwjgl.opengl.GL11.*;
+
+public class mapScreen {
+	public static int xCam = 0;
+	public static int yCam = 0;
+	public static int totalXCam = 0;
+	public static int totalYCam = 0;
+	
+	
+	public static void moveCam(int x, int y){
+		/*System.out.println("xCam: "+xCam+" yCam: "+yCam);
+		int newX = xCam;
+		int newY = yCam;
+		xCam = 0;
+		yCam = 0;
+		*/
+		
+		for(int i =0; i<MapMaker.tiles.size(); i++){
+			//System.out.println("this is another tes");
+			MapMaker.tiles.get(i).x+=x;
+			MapMaker.tiles.get(i).y+=y;
+		}
+		totalXCam +=x;
+		totalYCam +=y;
+		/*
+		newX = 0;
+		newY = 0;
+		totalXCam += newX;
+		totalYCam += newY;
+		*/
+	}
+
+}
